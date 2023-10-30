2be95e16f54f8c50af9b7c323fe2345765e71e1a
Feku_Sharma <fekusharma111@gmail.com>
2023-10-30 12:39:24 +0530

    Blog Listing

--- client/src/app/blogs/page.jsx
+++ client/src/app/blogs/page.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./page.module.css";
+import { DummyBlogPost } from "@/utils/constants";
+import BlogListing from "@/components/blogListing/BlogListing";
 
 const Blogs = () => {
-  return <div className={styles.container}>Blogs</div>;
+  return (
+    <div className={styles.container}>
+      <BlogListing blogs={DummyBlogPost.filter((blog) => blog.trending)} isTrending />
+      <BlogListing blogs={DummyBlogPost} />
+    </div>
+  );
 };
 
 export default Blogs;
---
+++ client/src/components/blogListing/BlogListing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./blogListing.module.css";
+import Image from "next/image";
+
+const BlogListing = ({ blogs, isTrending }) => (
+  <div className={styles.container}>
+    <h2 className={styles.sectionHeader}>{isTrending ? "TRENDING BLOGS" : "ALL BLOGS"}</h2>
+    <div className={styles.blogContainer}>
+      {blogs.map((blog) => (
+        <div key={blog.id} className={styles.item}>
+          <div className={styles.blogContent}>
+            <h3>{blog.title}</h3>
+            <p>{blog.description}</p>
+          </div>
+          <Image src={blog.image} alt="" width={200} height={100} />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default BlogListing;
