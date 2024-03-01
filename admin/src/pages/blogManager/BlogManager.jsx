0bc39b4703ef87b54730d6c2a26f92dc5734e368
Feku_Sharma <fekusharma111@gmail.com>
2024-03-01 22:09:18 +0530

    blogManager

--- admin/src/pages/blogManager/BlogManager.jsx
+++ admin/src/pages/blogManager/BlogManager.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeroSection from "../../components/DashboardSection/HeroSection";
 
 const BlogManager = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [nodata, setNoData] = useState(false);
+  const [allBlogData, setAllBlogData] = useState([]);
+
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+
+  const fetchBlogData = async () => {
+    try {
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+      // setLoading(false);
+      if (error.originalMessage) {
+        setError(error.originalMessage);
+      } else {
+        setError(error.message.message);
+      }
+    }
+  };
+
   return (
     <div>
       {/* <BlogManagerHeroGrid /> */}
